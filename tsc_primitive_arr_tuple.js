bcb26c6ac59baf9738988f35dceea77360e711cd
scKim <lansco1229@gmail.com>
2025-05-26 10:58:59 +0900

    Initial commit

---
+++ tsc_primitive_arr_tuple.js
@@ -0,0 +1,12 @@
+var userName; // 예: 이름
+var userAge; // 예: 나이
+var isAdmin; // 예: 관리자 여부
+userName = "Alice";
+userAge = 25;
+isAdmin = true;
+// 변수 선언과 초기값 지정
+var productName = "Apple"; // 상품 이름
+var productPrice = 100; // 상품 가격
+var isAvailable = true; // 상품 재고 여부
+// 예시 출력
+console.log("\uC0C1\uD488\uBA85: ".concat(productName, ", \uAC00\uACA9: ").concat(productPrice, ", \uC7AC\uACE0 \uC5EC\uBD80: ").concat(isAvailable));
---
+++ tsc_primitive_arr_tuple.ts
@@ -0,0 +1,15 @@
+let userName: string; // 예: 이름
+let userAge: number; // 예: 나이
+let isAdmin: boolean; // 예: 관리자 여부
+
+userName = "Alice";
+userAge = 25;
+isAdmin = true;
+
+// 변수 선언과 초기값 지정
+let productName: string = "Apple"; // 상품 이름
+let productPrice: number = 100; // 상품 가격
+let isAvailable: boolean = true; // 상품 재고 여부
+
+// 예시 출력
+console.log(`상품명: ${productName}, 가격: ${productPrice}, 재고 여부: ${isAvailable}`);
